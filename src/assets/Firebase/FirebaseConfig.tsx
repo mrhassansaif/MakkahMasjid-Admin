170519b73cfca9897623fe0d5e12e518be10803e
Muhammad Hassan Bin Saif <hassansaif0ki@gmail.com>
2024-01-24 11:59:27 +0500

    Added Firebase

---
+++ src/assets/Firebase/FirebaseConfig.tsx
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBdrqHN9n0DxZceVB68_LT3g2PxVkUEVXs",
+  authDomain: "makkahmasjid-73fec.firebaseapp.com",
+  projectId: "makkahmasjid-73fec",
+  storageBucket: "makkahmasjid-73fec.appspot.com",
+  messagingSenderId: "612902709295",
+  appId: "1:612902709295:web:906dd9e323318563253eda"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
